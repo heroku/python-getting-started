9b0dc25b18e337564ac6ac66a7aaef0c4fd79aff
Kenneth Reitz <me@kennethreitz.com>
2014-08-14 12:20:00 -0400

    db page

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', hello.views.index, name='index'),
+    url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 
 )
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Greeting
+
 # Create your views here.
 def index(request):
     return HttpResponse('Hello from Python!')
+
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'db.html', {'greetings': greetings})
+
