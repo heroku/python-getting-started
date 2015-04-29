236784a418c2dfe62c1800cf59dbe6234677e5a7
Rodolfo Carvalho <rhcarvalho@gmail.com>
2015-04-29 14:16:52 +0200

    Use SQLite when DATABASE_URL is not set

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -87,7 +87,9 @@ STATIC_URL = '/static/'
 
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
+db_config = dj_database_url.config()
+if db_config:
+    DATABASES['default'] =  db_config
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
