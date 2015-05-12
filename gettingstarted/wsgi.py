592dd43bd9c250f304219dcac9a91090298d67c5
Kenneth Reitz <me@kennethreitz.org>
2015-05-12 18:40:49 +0200

    update wsgi.py to use whitenoise

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -11,6 +11,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
