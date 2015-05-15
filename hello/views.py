5f86e082b82bf94b9dcff17d26cc10b617ff536b
Jon Mountjoy <tech@jonmountjoy.com>
2015-05-15 12:50:04 +0100

    first pass at adding the UI.  Still needs jQuery and fix for assets

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,8 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    # return HttpResponse('Hello from Python!')
+    return render(request, 'index.html')
 
 
 def db(request):
