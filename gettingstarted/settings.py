9cfa363b06a4506e4be6b0120cd4c154fd1aa148
Kenneth Reitz <me@kennethreitz.org>
2015-06-25 06:12:14 -0400

    cleanup settings

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -9,7 +9,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import dj_database_url
 
