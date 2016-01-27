0994779b974c733d588d9c7222321bec1bdb42f5
Kenneth Reitz <me@kennethreitz.org>
2016-01-27 17:05:04 -0500

    fix urlpattern usage

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,17 +1,16 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
 import hello.views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'gettingstarted.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+# Examples:
+# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
 
+urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
-
-)
+]
