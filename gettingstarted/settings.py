7b75d10e0d96e161520148e69b35ec4310345491
Kenneth Reitz <me@kennethreitz.org>
2016-02-04 22:53:33 -0500

    Remove BASE_DIR

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -124,7 +124,6 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
