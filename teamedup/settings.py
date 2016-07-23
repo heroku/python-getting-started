97a722dc93af7d46e19def5104d196b275bc1a65
nick byrne <nick@thinkactlive.com>
2016-07-23 15:52:23 +1000

    changed app name from hello > homepage

--- teamedup/settings.py
+++ teamedup/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'homepage'
 )
 
 MIDDLEWARE_CLASSES = (
--- teamedup/urls.py
+++ teamedup/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import homepage.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', homepage.views.index, name='index'),
+    url(r'^db', homepage.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
