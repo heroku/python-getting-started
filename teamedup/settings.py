cd46724b8c1182d30b8ed9598af48c4faa3509db
nick byrne <nick@thinkactlive.com>
2016-07-23 15:39:41 +1000

    changed django project names in relevant config files

--- teamedup/settings.py
+++ teamedup/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'gettingstarted.urls'
+ROOT_URLCONF = 'teamedup.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'gettingstarted.wsgi.application'
+WSGI_APPLICATION = 'teamedup.wsgi.application'
 
 
 # Database
