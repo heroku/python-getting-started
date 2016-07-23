b50e684de91b724ad942621b75ed2f20f20826bf
nick byrne <nick@thinkactlive.com>
2016-07-23 15:59:27 +1000

    added basic bootstrap theme to homepage index.html, using CDNs to serve static files

--- homepage/views.py
+++ homepage/views.py
@@ -6,7 +6,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello World!</h1>")
+    return render(request, 'index.html')
 
 
 def db(request):
