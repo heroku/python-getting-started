aac5ce9e8857f691aec5cf3f3e7ebd16fc0d855d
nick byrne <nick@thinkactlive.com>
2016-08-04 19:06:00 +0200

    testing s3 media file settings

--- teamedup/settings.py
+++ teamedup/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.sites',
     'django_comments',
+    'storages',
 )
 
 #SITE_ID for django.contrib.comments
@@ -143,7 +144,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(ENV_PATH, 'media/')
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
@@ -164,6 +165,7 @@ AWS_HEADERS = {
     'Cache-Control': 'max-age=94608000',
 }
 
+'''
 #AWS S3 BUCKET DETAILS
 AWS_STORAGE_BUCKET_NAME='teamedup'
 AWS_ACCESS_KEY_ID='AKIAJEHOEVOIQ2LZDEMQ'
@@ -177,9 +179,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
 # refers directly to STATIC_URL. So it's safest to always set it.
-STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-MEDIA_URL = STATIC_URL
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
 # you run `collectstatic`).
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-'''
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
