85fe21908d9466f63b38af913c839f0c7f544251
nick byrne <nick@thinkactlive.com>
2016-08-04 19:32:59 +0200

    updated people template to accommodate media files

--- teamedup/settings.py
+++ teamedup/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.media',
             ],
         },
     },
