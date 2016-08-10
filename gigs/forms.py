4624e6a7b17152d47b19bf2171ab56ca08668446
nick byrne <nick@thinkactlive.com>
2016-08-10 19:52:01 +0200

    resolved broken div issue within list.html (comments template)

--- gigs/forms.py
+++ gigs/forms.py
@@ -4,8 +4,10 @@ from .models import Gig
 class GigForm(forms.ModelForm):
 	class Meta:
 		model = Gig
-		fields = ('title', 'description')
+		fields = ('title', 'description', 'start_date', 'end_date')
 		widgets = {
 			'description': forms.Textarea(attrs={'class' : 'form-control'}),
-			'title': forms.TextInput(attrs={'class' : 'form-control'})
+			'title': forms.TextInput(attrs={'class' : 'form-control'}),
+			'start_date': forms.DateInput(),
+			'end_date': forms.DateInput(),
 		}
