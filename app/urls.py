8be1bc2c7686ddb8cd20be06395aeda7fc171bbc
Dmitry Ogurtsov <zorbis11@gmail.com>
2016-10-07 10:52:54 +0300

    working on sign up form

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^login_user/$', views.login_user, name='login_user'),
     url(r'^login/$', views.login_view, name='login_view'),
     url(r'^profile/$', views.edit_profile, name='edit_profile'),
-    url(r'^signup$', views.signup, name='signup'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
