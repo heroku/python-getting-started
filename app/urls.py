f9603f9e2c04dc518895c1a183d67784fe69a293
Dmitry Ogurtsov <zorbis11@gmail.com>
2016-10-09 20:31:46 +0300

    Now the organizations and user is properly created on sign up

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login_user/$', views.login_user, name='login_user'),
+    url(r'^login_user/$', views.login_user, name='login_user'), # TODO: url and view names are confusing
     url(r'^login/$', views.login_view, name='login_view'),
     url(r'^profile/$', views.edit_profile, name='edit_profile'),
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^signout/$', views.sign_out, name='sign-out'),
+    url(r'^activate/(?P<token>\w+)/$', views.activate_account, name='activate-account'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,19 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.utils.translation import ugettext as _
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
+from django.core.mail import send_mail
 
 from .models import Alert
+from app.models import Organization, OrganizationMember, Token
 from teams.models import Team
-from teams.forms import OrgSignUp
-
+from teams.forms import OrgSignUp # TODO: move form to 'app'
+from teamedup import settings
 
 @login_required
 def index(request):
@@ -28,7 +33,7 @@ def login_user(request):
     username = password = ''
 
     if request.POST:
-        username = request.POST['username']
+        username = request.POST['username'] # TODO: use email instead of username
         password = request.POST['password']
 
         user = authenticate(username=username, password=password)
@@ -76,6 +81,43 @@ def signup(request):
     if request.method == 'POST':
         form = OrgSignUp(request.POST)
         if form.is_valid():
-            print 'user created'
-        
+            user = User(email=form.cleaned_data.get('email'), username=form.cleaned_data.get('email'))
+            user.set_password(form.cleaned_data.get('password'))
+            user.save()
+            org = Organization(name=form.cleaned_data.get('organization_name'))
+            org.save()
+            _orgmem = OrganizationMember(user=user, is_owner=True, organization=org).save()
+            token = Token(user=user, type='signup')
+            token.save()
+            link = request.build_absolute_uri(reverse('activate-account', args=[token.token]))
+
+            print('Activation link: %s' % (link))
+
+            send_mail('Account activation', settings.ACCOUNT_ACTIVATION_EMAIL % (org.name, link,),
+                      'support@teamedup.com', [user.email, ])
+            messages.add_message(request, messages.SUCCESS,
+                                 _('Please click the activation link which was sent to your email'))
+            _user = authenticate(username=form.cleaned_data['email'], password=form.cleaned_data['password'])
+            if _user is not None: # TODO: only sign in users which activated their account
+                login(request, _user)
+                return redirect('/app/')
+            return redirect('/')
     return render(request, 'app/signup.html', locals())
+
+
+def activate_account(request, token):
+    try:
+        token = Token.objects.get(token=token, expired=False, type='signup')
+    except Token.DoesNotExist:
+        raise Http404
+
+    token.user.is_active = True
+    token.user.save()
+    messages.add_message(request, messages.SUCCESS, _('Your account is now active'))
+    return redirect('sign-in')
+
+
+def sign_out(request):
+    logout(request)
+    messages.add_message(request, messages.SUCCESS, _('You have successfully logged out'))
+    return redirect(reverse('login_view'))
