954a99bbd13a47ffc8369bf33af68364488c3904
Robbie Cruse <robbiecrusedev@outlook.com>
2016-10-10 11:12:57 +1100

    remove notification model, update invite model

--- teams/views.py
+++ teams/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from teams.models import Team, Role, Member, Invite
-from app.models import Notification
 from django.contrib.auth.models import User
 
 from datetime import datetime, timedelta
@@ -90,11 +89,7 @@ def invite_people(request, team_id):
 def _invite_people(request, team):
     invitees = [User.objects.get(pk=invitee_id) for invitee_id in request.POST.get('invitees').split(",")]
     for invitee in invitees:
-        invitation = Invite(team=team, inviter=request.user, invitee=invitee, status='created')
+        invitation = Invite(team=team, inviter=request.user, invitee=invitee, status='created', expired_at=datetime.now()+timedelta(days=7), read=False)
         invitation.save()
-        now = datetime.now()
-        expiredAt = now + timedelta(days=7)
-        notification = Notification(activity='invite_people_to_team', activityId=invitation.id, expiredAt=expiredAt, read=False)
-        notification.save()
 
     return HttpResponse(status=200)
