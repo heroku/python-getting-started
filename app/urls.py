cc02ea264e37e9c095764467b65f3670f2ebc98a
Dmitry Ogurtsov <zorbis11@gmail.com>
2016-10-11 23:16:51 +0300

    Now the system messages are displayed (in a standard django way)

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login_user/$', views.login_user, name='login_user'), # TODO: url and view names are confusing
     url(r'^login/$', views.login_view, name='login_view'),
     url(r'^settings/$', views.edit_settings, name='settings'),
     url(r'^signup/$', views.signup, name='signup'),
