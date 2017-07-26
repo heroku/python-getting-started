91c05703aff13abff5aaa2f6ffe43afb953658f8
Kenneth Reitz <me@kennethreitz.org>
2017-07-26 17:57:44 -0400

    update test database name

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+DATABASES['default']['TEST'] = {'NAME': DATABASES['default']['NAME']}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
