f56b90499ec11e1c9576da2f8c7331300e189db3
Kenneth Reitz <me@kennethreitz.org>
2017-12-11 12:01:49 -0500

    fix urls for django 2.0
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.urls import path
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,5 +13,5 @@ import hello.views
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
-    url(r'^admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
 ]
