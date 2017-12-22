165f36c46f7b36bab456653d7870ae5aeac14038
ludwig125 <shayakaw@yahoo-corp.jp>
2017-12-23 00:07:27 +0900

    Demo

--- hello/bak_views.py
+++ hello/bak_views.py
@@ -1,17 +1,12 @@
-import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
 
-## Create your views here.
-#def index(request):
-#    # return HttpResponse('Hello from Python!')
-#    return render(request, 'index.html')
+# Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    # return HttpResponse('Hello from Python!')
+    return render(request, 'index.html')
 
 
 def db(request):
