f00aaaaf9aaa3c852241182febaebef0e30b4b84
ludwig125 <shayakaw@yahoo-corp.jp>
2017-12-23 00:16:08 +0900

    hello

--- hello/views.py
+++ hello/views.py
@@ -5,13 +5,13 @@ from django.http import HttpResponse
 from .models import Greeting
 
 ## Create your views here.
-#def index(request):
-#    # return HttpResponse('Hello from Python!')
-#    return render(request, 'index.html')
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return HttpResponse('Hello from Python!')
+#    return render(request, 'index.html')
+#def index(request):
+#    r = requests.get('http://httpbin.org/status/418')
+#    print(r.text)
+#    return HttpResponse('<pre>' + r.text + '</pre>')
 
 
 def db(request):
