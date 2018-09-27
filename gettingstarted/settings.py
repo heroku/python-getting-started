d507d494731419880c8e290dcd860efb5f6b650a
python-force <studio@pythonforce.com>
2018-09-27 17:51:47 -0400

    removing impor django_heroku

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -121,5 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-django_heroku.settings(locals())
