dd8436a9dbe6e208697e809aef9925034aa4fa7b
Casey Faist <caseylfaist@gmail.com>
2018-10-18 16:27:33 -0500

    update static root for whitenoise

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -76,8 +76,6 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-import dj_database_url
-
 DATABASES = {
     "default": {
         "ENGINE" : "django.db.backends.sqlite3",
@@ -115,6 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
