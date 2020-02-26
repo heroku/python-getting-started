90de29ce3771ac28d86660f708fa1804f96e0ad9
Guilherme Gonçalves <gonc.guilherme@gmail.com>
2020-02-26 13:24:33 -0300

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -9,7 +9,8 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     times = int(os.environ.get('TIMES', 3))
-    return HttpResponse('Hello! ' * times)
+    return HttpResponse('''<script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/40552c01-2431-44a2-9bb6-ead4fbb70f44-loader.js" ></script>
+                        <h1>Hello!</h1>''')
 
 
 def db(request):
