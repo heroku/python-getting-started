ca246b3266701a709f4c1ed7fc5efbe7b45d7106
Guilherme Gonçalves <gonc.guilherme@gmail.com>
2020-02-26 13:26:54 -0300

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -10,7 +10,7 @@ from .models import Greeting
 def index(request):
     times = int(os.environ.get('TIMES', 3))
     return HttpResponse('''<script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/40552c01-2431-44a2-9bb6-ead4fbb70f44-loader.js" ></script>
-                        <h1>Hello!</h1>''')
+                        <h1>Helloo!</h1>''')
 
 
 def db(request):
