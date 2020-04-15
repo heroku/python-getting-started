ee1624bb4f0f81953a7c531818aa9fd5a32baa79
srishti1616 <63519406+srishti1616@users.noreply.github.com>
2020-04-15 00:13:21 -0400

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -5,8 +5,9 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 
 def db(request):
