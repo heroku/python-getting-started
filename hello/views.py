37ec703205532b5e9f9c342dad7b41ba740bf904
djazi <52473469+djazi@users.noreply.github.com>
2020-06-20 04:29:14 +0100

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 
 def db(request):
