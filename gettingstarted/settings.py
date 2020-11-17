c2dd2dcd153a9cbcd49b54daa48d380fb08344b4
Jean-Baptiste Courtois <jean-baptiste.courtois@sap.com>
2020-11-17 19:26:37 +0100

    Update django_heroku -> django_to_heroku

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+import django_on_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -116,4 +116,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
