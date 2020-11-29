232a067b6793dda0826022d177d05294a10af314
Jean-Baptiste Courtois <jean-baptiste.courtois@sap.com>
2020-11-29 18:13:10 +0100

    Use whitenoise

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "gettingstarted.urls"
