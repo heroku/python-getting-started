da47d9cfd988933c3693e7ac2faca8416e3ec822
Jean-Baptiste Courtois <jean-baptiste.courtois@sap.com>
2020-11-29 03:11:00 +0100

    Drop django-heroku

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import os
-import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -107,5 +106,3 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
-
-django_heroku.settings(locals())
