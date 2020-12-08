e8531f230c011d046a31fe90f8634a9f00aff3bc
OferDrori <53820089+OferDrori@users.noreply.github.com>
2020-12-08 14:13:51 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
-import os
-import sys
+from flask import Flask
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'hello test'
+if __name__ == '__main__':
+    app.run()
