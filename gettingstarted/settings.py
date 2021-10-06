cda03ed7bf065f6ea6e519c0065cc91b1bfbc212
Jean-Baptiste Courtois <jean-baptiste.courtois@sap.com>
2021-10-06 14:25:33 +0200

    Configure login view

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = "gettingstarted.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
