2955882c2360e79c02c796ce9cdfdd7822de5909
Lynn Watson <lynn@lswatson.com>
2021-10-28 13:38:15 -0600

    Demo

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 from .models import Greeting
 
 # Create your views here.
+"""
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
+"""
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 
 def db(request):
