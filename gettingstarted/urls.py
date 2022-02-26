9e889487bfc6ee8132b1f3b5f1a14e14e9fe8bcd
Xavier <xaviertuominen@gmail.com>
2022-02-26 10:09:02 -0600

    as

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -17,7 +17,7 @@ import hello.views
 urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
-    path(r'^(?!)city/$', hello.views.city), #, name="city"),
+    path(r'^(?!)city/', hello.views.city), #, name="city"),
 
     
     path("admin/", admin.site.urls),
