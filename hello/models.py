435625fa30ad3f7b3e0c424e0959b7c355f912bd
Mission0466 <krishnachaitanya0466@outlook.com>
2022-05-01 18:39:55 +0530

    upd

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'db.html', {'greetings': greetings})
--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 from .models import Greeting
+import os
 
 # Create your views here.
 def index(request):
@@ -22,3 +23,7 @@ def index(request):
     r = requests.get('https://httpbin.org/status/418')
     print(r.text)
     return HttpResponse('<pre>' + r.text + '</pre>')
+
+def index(request):
+    times = int(os.environ.get('TIMES', 3))
+    return HttpResponse('Hello! ' * times)
