865177dc008530285353334acc4e4b0413601af9
Xavier <xaviertuominen@gmail.com>
2022-05-03 09:15:09 -0500

    1

--- hello/settings.py
+++ hello/settings.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "livereload",
     "django.contrib.staticfiles",
     "hello",
 ]
@@ -39,7 +38,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "livereload.middleware.LiveReloadScript",
 ]
 
 ROOT_URLCONF = "gettingstarted.urls"
