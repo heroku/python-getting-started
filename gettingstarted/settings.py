8968654f8517ea2e58d227bdbf549cdf6fac080a
Ferran Boladeres Salvado <FBoladeres@picis.com>
2022-06-10 17:24:12 +0200

    Demo

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -18,6 +18,7 @@ import django_heroku
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -28,6 +29,8 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+CSRF_TRUSTED_ORIGINS = ['https://web.postman.co']
 
 
 # Application definition
