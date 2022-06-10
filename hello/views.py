745bbf5e4ac83819658b22a7ac1cbb52f7191c0e
Ferran Boladeres Salvado <FBoladeres@picis.com>
2022-06-10 17:27:53 +0200

    Demo

--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 
 from .models import Greeting
 import requests
 
 # Create your views here.
+@csrf_protect
 def index(request):
     #r = requests.get('https://httpbin.org/status/418')
     #print(r.text)
