008bd201c8b8500c5199af1a778e756a0d57c193
Prateek Sane <prateek.sane@salesforce.com>
2022-07-13 14:41:47 -0700

    small formatting changes

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku 
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
