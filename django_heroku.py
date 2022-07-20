e553515f0768f00310aa15c9053fffa236c9e13d
Prateek Sane <prateek.sane@salesforce.com>
2022-07-20 10:07:39 -0700

    remove logger and db colors (multiple databases)

--- django_heroku.py
+++ django_heroku.py
@@ -6,8 +6,6 @@ from django.test.runner import DiscoverRunner
 
 MAX_CONN_AGE = 600
 
-logger = logging.getLogger(__name__)
-
 
 class HerokuDiscoverRunner(DiscoverRunner):
     """Test Runner for Heroku CI, which provides a database for you.
@@ -41,7 +39,8 @@ class HerokuDiscoverRunner(DiscoverRunner):
         for connection, old_name, destroy in old_config:
             if destroy:
                 self._wipe_tables(connection)
-        super(HerokuDiscoverRunner, self).teardown_databases(old_config, **kwargs)
+        super(HerokuDiscoverRunner, self).teardown_databases(
+            old_config, **kwargs)
 
 
 def settings(config, *, db_colors=False, databases=True, test_runner=True, staticfiles=True, allowed_hosts=True, logging=True, secret_key=True):
@@ -54,32 +53,15 @@ def settings(config, *, db_colors=False, databases=True, test_runner=True, stati
             config['DATABASES'] = {'default': None}
 
         conn_max_age = config.get('CONN_MAX_AGE', MAX_CONN_AGE)
-            
-        if db_colors:
-            # Support all Heroku databases.
-            # TODO: This appears to break TestRunner.
-            for (env, url) in os.environ.items():
-                if env.startswith('HEROKU_POSTGRESQL'):
-                    db_color = env[len('HEROKU_POSTGRESQL_'):].split('_')[0]
-
-                    logger.info('Adding ${} to DATABASES Django setting ({}).'.format(env, db_color))
-
-                    config['DATABASES'][db_color] = dj_database_url.parse(url, conn_max_age=conn_max_age, ssl_require=True)
 
         if 'DATABASE_URL' in os.environ:
-            logger.info('Adding $DATABASE_URL to default DATABASE Django setting.')
-
             # Configure Django for DATABASE_URL environment variable.
-            config['DATABASES']['default'] = dj_database_url.config(conn_max_age=conn_max_age, ssl_require=True)
-
-            logger.info('Adding $DATABASE_URL to TEST default DATABASE Django setting.')
+            config['DATABASES']['default'] = dj_database_url.config(
+                conn_max_age=conn_max_age, ssl_require=True)
 
             # Enable test database if found in CI environment.
             if 'CI' in os.environ:
                 config['DATABASES']['default']['TEST'] = config['DATABASES']['default']
-
-        else:
-            logger.info('$DATABASE_URL not found, falling back to previous settings!')
 
     if test_runner:
         # Enable test runner if found in CI environment.
@@ -88,8 +70,6 @@ def settings(config, *, db_colors=False, databases=True, test_runner=True, stati
 
     # Staticfiles configuration.
     if staticfiles:
-        logger.info('Applying Heroku Staticfiles configuration to Django settings.')
-
         config['STATIC_ROOT'] = os.path.join(config['BASE_DIR'], 'staticfiles')
         config['STATIC_URL'] = '/static/'
 
@@ -98,19 +78,17 @@ def settings(config, *, db_colors=False, databases=True, test_runner=True, stati
 
         # Insert Whitenoise Middleware.
         try:
-            config['MIDDLEWARE_CLASSES'] = tuple(['whitenoise.middleware.WhiteNoiseMiddleware'] + list(config['MIDDLEWARE_CLASSES']))
+            config['MIDDLEWARE_CLASSES'] = tuple(
+                ['whitenoise.middleware.WhiteNoiseMiddleware'] + list(config['MIDDLEWARE_CLASSES']))
         except KeyError:
-            config['MIDDLEWARE'] = tuple(['whitenoise.middleware.WhiteNoiseMiddleware'] + list(config['MIDDLEWARE']))
+            config['MIDDLEWARE'] = tuple(
+                ['whitenoise.middleware.WhiteNoiseMiddleware'] + list(config['MIDDLEWARE']))
 
         # Enable GZip.
         config['STATICFILES_STORAGE'] = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     if allowed_hosts:
-        logger.info('Applying Heroku ALLOWED_HOSTS configuration to Django settings.')
         config['ALLOWED_HOSTS'] = ['*']
-
-    if logging:
-        logger.info('Applying Heroku logging configuration to Django settings.')
 
         config['LOGGING'] = {
             'version': 1,
@@ -148,6 +126,5 @@ def settings(config, *, db_colors=False, databases=True, test_runner=True, stati
     # SECRET_KEY configuration.
     if secret_key:
         if 'SECRET_KEY' in os.environ:
-            logger.info('Adding $SECRET_KEY to SECRET_KEY Django setting.')
             # Set the Django setting from the environment variable.
             config['SECRET_KEY'] = os.environ['SECRET_KEY']
