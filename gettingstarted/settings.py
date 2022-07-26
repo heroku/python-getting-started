8ad4797512e8540518254233b1d44b38833114c2
Prateek Sane <prateek.sane@salesforce.com>
2022-07-26 11:17:14 -0700

    remove logging config

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -162,37 +162,3 @@ try:
 except KeyError:
     config['MIDDLEWARE'] = tuple(
         ['whitenoise.middleware.WhiteNoiseMiddleware'] + list(config['MIDDLEWARE']))
-
-
-config['LOGGING'] = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
-                       'pathname=%(pathname)s lineno=%(lineno)s ' +
-                       'funcname=%(funcName)s %(message)s'),
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'testlogger': {
-            'handlers': ['console'],
-            'level': 'INFO',
-        }
-    }
-}
