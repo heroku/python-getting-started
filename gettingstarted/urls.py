317c8654fc841688a0af2e588bc770e5c523586b
Ed Morley <501702+edmorley@users.noreply.github.com>
2023-05-19 15:50:50 +0100

    Port changes from the upstream `startproject` template (#194)
    This guide is intentionally closely based around the template project
    generated by Django's `startproject` and `startapp` commands.
    
    This ports changes from the upstream template that were made in
    the last few versions of Django.
    
    The upstream template content was generated using:
    ```
    django-admin startproject gettingstarted
    cd gettingstarted
    ./manage.py startapp hello
    ```

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,17 +1,23 @@
+"""
+URL configuration for gettingstarted project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path
 
-admin.autodiscover()
-
 import hello.views
-
-# To add a new path, first import the app:
-# import blog
-#
-# Then add the new path:
-# path('blog/', blog.urls, name="blog")
-#
-# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
