0cb989c8444bea6a087d4d5dfa637e8c062ad051
Ed Morley <501702+edmorley@users.noreply.github.com>
2023-05-19 14:59:05 +0100

    Improve example unit tests
    The example unit tests have been rewritten to actually test the app,
    and so they no longer use unnecessary setup boilerplate.

--- hello/tests.py
+++ hello/tests.py
@@ -1,19 +1,26 @@
-from django.contrib.auth.models import AnonymousUser
-from django.test import TestCase, RequestFactory
+from django.test import TestCase
 
-from .views import index
+# Create your tests here.
 
 
-class SimpleTest(TestCase):
-    def setUp(self):
-        # Every test needs access to the request factory.
-        self.factory = RequestFactory()
+# Note: The tests below rely upon static assets (for the rendered templates), so require that either:
+# 1. The static assets have been processed - ie: `./manage.py collectstatic` has been run.
+# 2. Or, the tests are run in debug mode (which means WhiteNoise will use auto-refresh mode),
+#    using: `./manage.py test --debug-mode`
+class ExampleTest(TestCase):
+    def test_index_page(self):
+        response = self.client.get("/")
+        self.assertContains(
+            response, "Getting Started with Python on Heroku", status_code=200
+        )
 
-    def test_details(self):
-        # Create an instance of a GET request.
-        request = self.factory.get("/")
-        request.user = AnonymousUser()
+    def test_db_page(self):
+        # Each time the page is requested, the number of recorded greetings increases.
 
-        # Test my_view() as if it were deployed at /customer/details
-        response = index(request)
-        self.assertEqual(response.status_code, 200)
+        first_response = self.client.get("/db/")
+        self.assertEqual(first_response.status_code, 200)
+        self.assertEqual(len(first_response.context["greetings"]), 1)
+
+        second_response = self.client.get("/db/")
+        self.assertEqual(second_response.status_code, 200)
+        self.assertEqual(len(second_response.context["greetings"]), 2)
