02eb1411331cd23c7e692887cf317caba5363557
Ed Morley <501702+edmorley@users.noreply.github.com>
2023-05-19 13:04:43 +0100

    Format the codebase using Black (#188)
    https://github.com/psf/black
    
    (This isn't being enforced in CI, since this project is a template, so we
    can't add a bunch of opinionated CI checks etc)
    
    GUS-W-13312373.

--- hello/views.py
+++ hello/views.py
@@ -4,13 +4,14 @@ from django.http import HttpResponse
 from .models import Greeting
 
 # Create your views here.
+
+
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
 
 def db(request):
-
     greeting = Greeting()
     greeting.save()
 
