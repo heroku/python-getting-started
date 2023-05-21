c99a168fa1c9e6277a986c0d86606f0106fdf843
Ed Morley <501702+edmorley@users.noreply.github.com>
2023-05-21 22:27:37 +0100

    Don't provision a Postgres database by default (#203)
    * The app no longer provisions a Postgres addon by default (since the Postgres
       adapter dependency in `requirements.txt` is commented out, which is what
       the Python buildpack uses to determine whether to auto-provision), so that it's
       possible to try the main part of the guide without incurring any additional
      charges beyond the eco dyno plan.
    * The Postgres DB addon has been removed from `app.json` too, which prevents
       it from being auto-provisioned for Review Apps too.
    * Help comments has been added to the `db` view function, to try and help prevent
       confusion due to errors on Heroku from the DB not being auto-provisioned.
    * The Postgres adapter has been switched from `psycopg2` to `psycopg`, which
       counter-intuitively is newer (`psycopg` is the v3 implementation of the adapter).
       The binary version of the package is now also used when not in production, to speed
       up local installation and reduce the chance of running into library/header issues.
    * The new Django DB connection health check feature has been enabled.
    * The unnecessary `HerokuDiscoverRunner` custom test runner and database`TEST`
       options have been removed.
    * An example `release` process entry has been added (commented out) to `Procfile`,
       which runs the Django DB migrations using the Heroku Release Phase feature.
       
    GUS-W-13152716.

--- Procfile
+++ Procfile
@@ -1,1 +1,7 @@
 web: gunicorn gettingstarted.wsgi
+
+# Uncomment this `release` process if you are using a database, so that Django's model
+# migrations are run as part of app deployment, using Heroku's Release Phase feature:
+# https://docs.djangoproject.com/en/4.2/topics/migrations/
+# https://devcenter.heroku.com/articles/release-phase
+#release: ./manage.py migrate --no-input
--- hello/views.py
+++ hello/views.py
@@ -10,6 +10,16 @@ def index(request):
 
 
 def db(request):
+    # If you encounter errors visiting the `/db/` page on the example app, check that:
+    #
+    # When running the app on Heroku:
+    #   1. You have added the Postgres database to your app.
+    #   2. You have uncommented the `psycopg` dependency in `requirements.txt`, and the `release`
+    #      process entry in `Procfile`, git committed your changes and re-deployed the app.
+    #
+    # When running the app locally:
+    #   1. You have run `./manage.py migrate` to create the `hello_greeting` database table.
+
     greeting = Greeting()
     greeting.save()
 
