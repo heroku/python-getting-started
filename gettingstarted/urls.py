b048ae113deb54b38373b9582df403375ccc4846
Ed Morley <501702+edmorley@users.noreply.github.com>
2024-08-09 10:41:23 +0100

    Re sync with upstream Django 5.1 `createproject` output

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -2,7 +2,7 @@
 URL configuration for gettingstarted project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 # from django.contrib import admin
 from django.urls import path
 
@@ -23,6 +24,6 @@ urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     # Uncomment this and the entry in `INSTALLED_APPS` if you wish to use the Django admin feature:
-    # https://docs.djangoproject.com/en/5.0/ref/contrib/admin/
+    # https://docs.djangoproject.com/en/5.1/ref/contrib/admin/
     # path("admin/", admin.site.urls),
 ]
