701a44ef6b7e4c7508f3a978488e0c48c60f046b
Timm Kaptein <timm.kaptein@stadlerrail.com>
2025-03-25 08:07:03 +0100

    change names

---
+++ config/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+# from django.contrib import admin
+from django.urls import path
+
+import webapp.views
+
+urlpatterns = [
+    path("", webapp.views.index, name="index"),
+    path("db/", webapp.views.db, name="db"),
+    # Uncomment this and the entry in `INSTALLED_APPS` if you wish to use the Django admin feature:
+    # https://docs.djangoproject.com/en/5.1/ref/contrib/admin/
+    # path("admin/", admin.site.urls),
+]
