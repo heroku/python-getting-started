f7af23f457b76c365c20540bcbdfef4935f61966
Dezik <dawparuzel@gmail.com>
2025-12-19 00:49:41 +0100

    Switch to PostgreSQL and add auto-migrations

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -133,7 +133,7 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-if IS_HEROKU_APP:
+#if IS_HEROKU_APP:
     # In production on Heroku the database configuration is derived from the `DATABASE_URL`
     # environment variable by the dj-database-url package. `DATABASE_URL` will be set
     # automatically by Heroku when a database addon is attached to your Heroku app. See:
