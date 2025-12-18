7fe22d509cda40dbd6d88cf54bbf97e7bce0777e
Dezik <dawparuzel@gmail.com>
2025-12-18 16:27:13 +0100

    dasdaa

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -44,26 +44,19 @@ SECRET_KEY = os.environ.get(
 # Debug mode will be automatically enabled when the project is run via `heroku local` (which
 # loads the environment variables set in the `.env` file, where `ENVIRONMENT=development`).
 # SECURITY WARNING: Don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("ENVIRONMENT") == "development"
 
 # The `DYNO` env var is set on Heroku CI, but it's not a real Heroku app, so we have to
 # also explicitly exclude CI:
 # https://devcenter.heroku.com/articles/heroku-ci#immutable-environment-variables
 IS_HEROKU_APP = "DYNO" in os.environ and "CI" not in os.environ
-# Zawsze bazowa lista hostów (lokalnie + Render)
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'python-getting-started-lab1-lk4l.onrender.com' , 'https://python-getting-started-lab1-lk4l.onrender.com/']
-# Jeśli używasz Heroku i chcesz wildcard, możesz opcjonalnie rozszerzyć:
-if os.environ.get('IS_HEROKU_APP') == '1':
-    ALLOWED_HOSTS.append('*')
-    # Dynamicznie z env (dla przyszłości, np. ALLOWED_HOSTS z Render)
-if os.environ.get('ALLOWED_HOSTS'):
-    ALLOWED_HOSTS.extend(os.environ.get('ALLOWED_HOSTS').split(','))
+
 if IS_HEROKU_APP:
     # On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS`, since the Heroku router performs
     # validation of the Host header in the incoming HTTP request. On other platforms you may need to
     # list the expected hostnames explicitly in production to prevent HTTP Host header attacks. See:
     # https://docs.djangoproject.com/en/6.0/ref/settings/#std-setting-ALLOWED_HOSTS
-    #ALLOWED_HOSTS = ["*"]
+    ALLOWED_HOSTS = ["*"]
 
     # Redirect all non-HTTPS requests to HTTPS. This requires that:
     # 1. Your app has a TLS/SSL certificate, which all `*.herokuapp.com` domains do by default.
@@ -77,7 +70,7 @@ if IS_HEROKU_APP:
     # https://docs.djangoproject.com/en/6.0/ref/middleware/#http-strict-transport-security
     SECURE_SSL_REDIRECT = True
 else:
-    ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0", "[::]"]
+    ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
